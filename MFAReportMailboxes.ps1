846a95bed77bc975f1ba7e76502e359d2fe019d4
Tony Redmond <45732130+12Knocksinna@users.noreply.github.com>
2018-12-09 19:18:29 +0000

    Add files via upload
    Report mailboxes and the last time they were processed by MFA

---
+++ MFAReportMailboxes.ps1
@@ -0,0 +1,20 @@
+# MFAReportMailboxes.ps1
+# List mailboxes and the last time MFA processed each mailbox
+$Mbx = Get-Mailbox -RecipientTypeDetails UserMailbox -ResultSize Unlimited
+$Report = @()
+ForEach ($M in $Mbx) {
+   $LastProcessed = $Null
+   Write-Host "Processing" $M.DisplayName
+   $Log = Export-MailboxDiagnosticLogs -Identity $M.Alias -ExtendedProperties
+   $xml = [xml]($Log.MailboxLog)  
+   $LastProcessed = ($xml.Properties.MailboxTable.Property | ? {$_.Name -like "*ELCLastSuccessTimestamp*"}).Value   
+   $ItemsDeleted  = $xml.Properties.MailboxTable.Property | ? {$_.Name -like "*ElcLastRunDeletedFromRootItemCount*"}
+   If ($LastProcessed -eq $Null) {
+      $LastProcessed = "Not processed"}
+   $ReportLine = [PSCustomObject][Ordered]@{
+           User          = $M.DisplayName
+           LastProcessed = $LastProcessed
+           ItemsDeleted  = $ItemsDeleted.Value}      
+    $Report += $ReportLine
+  }
+$Report | Select User, LastProcessed, ItemsDeleted
