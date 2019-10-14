4bcd6d83d791baac960ca5397b4b95e477bf9c7c
Tony Redmond <45732130+12Knocksinna@users.noreply.github.com>
2019-10-14 17:23:25 +0100

    Create FindObsoleteGuestsByActivity.ps1
    A script to find Azure AD guest accounts that are potentially obsolete based on data from the Office 365 audit log and message trace data

---
+++ FindObsoleteGuestsByActivity.ps1
@@ -0,0 +1,41 @@
+# Script to find guest accounts that are inactive
+$Guests = (Get-AzureADUser -Filter "UserType eq 'Guest'" -All $True| Select Displayname, UserPrincipalName, Mail, RefreshTokensValidFromDateTime)
+Write-Host $Guests.Count "guest accounts found. Checking their recent activity..."
+$StartDate = (Get-Date).AddDays(-90) #For audit log
+$StartDate2 = (Get-Date).AddDays(-10) #For message trace
+$EndDate = (Get-Date); $Active = 0; $EmailActive = 0; $Inactive = 0; $AuditRec = 0; $Report = @()
+ForEach ($G in $Guests) {
+    Write-Host "Checking" $G.DisplayName  
+    $LastAuditAction = $Null; $LastAuditRecord = $Null
+    # Search for audit records for this user
+    $Recs = (Search-UnifiedAuditLog -UserIds $G.Mail, $G.UserPrincipalName -Operations UserLoggedIn, SecureLinkUsed, TeamsSessionStarted -StartDate $StartDate -EndDate $EndDate -ResultSize 1)
+    If ($Recs.CreationDate -ne $Null) { # We found some audit logs
+       $LastAuditRecord = $Recs[0].CreationDate; $LastAuditAction = $Recs[0].Operations; $AuditRec++
+       Write-Host "Last audit record for" $G.DisplayName "on" $LastAuditRecord "for" $LastAuditAction -Foregroundcolor Green }
+    Else { Write-Host "No audit records found in the last 90 days for" $G.DisplayName "; account created on" $G.RefreshTokensValidFromDateTime -Foregroundcolor Red } 
+    # Check message trace data because guests might receive email through membership of Outlook Groups. Email address must be valid for the check to work
+    If ($G.Mail -ne $Null) {
+       $EmailRecs = (Get-MessageTrace –StartDate $StartDate2 –EndDate $EndDate -Recipient $G.Mail)            
+       If ($EmailRecs.Count -gt 0) {
+           Write-Host "Email traffic found for" $G.DisplayName "at" $EmailRecs[0].Received -foregroundcolor Yellow
+           $EmailActive++ }}
+     # Write out report line     
+     $ReportLine = [PSCustomObject][Ordered]@{ 
+          Guest            = $G.Mail
+          Name             = $G.DisplayName
+          Created          = $G.RefreshTokensValidFromDateTime 
+          EmailCount       = $EmailRecs.Count
+          LastConnectOn    = $LastAuditRecord
+          LastConnect      = $LastAuditAction} 
+       $Report += $ReportLine  }          
+ 
+$Active = $AuditRec + $EmailActive
+$Report | Export-CSV -NoTypeInformation c:\temp\GuestActivity.csv      
+Write-Host ""
+Write-Host "Statistics"
+Write-Host "----------"
+Write-Host "Guest Accounts          " $Guests.Count
+Write-Host "Active Guests           " $Active
+Write-Host "Audit Record foun       " $AuditRec
+Write-Host "Active on Email         " $EmailActive
+Write-Host "InActive Guests         " ($Guests.Count - $Active)
