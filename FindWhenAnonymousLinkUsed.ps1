a7a54fb05b8d034be218cfa4fdc2b967ad8bda50
Tony Redmond <45732130+12Knocksinna@users.noreply.github.com>
2019-10-17 12:49:24 +0100

    Update FindWhenAnonymousLinkUsed.ps1
    Added progress bar

--- FindWhenAnonymousLinkUsed.ps1
+++ FindWhenAnonymousLinkUsed.ps1
@@ -1,29 +1,32 @@
-# Find out when an anonymous link is used
-$StartDate = (Get-Date).AddDays(-90); $EndDate = (Get-Date)
+# Find out when an anonymous link is used by someone outside an Office 365 tenant to access SharePoint Online and OneDrive for Business documents
+$StartDate = (Get-Date).AddDays(-90); $EndDate = (Get-Date) #Maximum search range for audit log for E3 users
+CLS; Write-Host "Searching Office 365 Audit Records to find anonymous sharing activity"
 $Records = (Search-UnifiedAuditLog -Operations AnonymousLinkUsed -StartDate $StartDate -EndDate $EndDate -ResultSize 1000)
 If ($Records.Count -eq 0) {
     Write-Host "No anonymous share records found." }
 Else {
-    Write-Host "Processing" $Records.Count "audit records..."}
-# Create output file for report
-$Report = @()
-# Scan each audit record to extract information
-ForEach ($Rec in $Records) {
-  $AuditData = ConvertFrom-Json $Rec.Auditdata
-  $ReportLine = [PSCustomObject][Ordered]@{
-    TimeStamp = Get-Date($AuditData.CreationTime) -format g
-    User      = $AuditData.UserId
-    Action    = $AuditData.Operation
-    Object    = $AuditData.ObjectId
-    IPAddress = $AuditData.ClientIP
-    Workload  = $AuditData.Workload
-    Site      = $AuditData.SiteUrl
-    FileName  = $AuditData.SourceFileName 
-    SortTime  = $AuditData.CreationTime }
-  $Report += $ReportLine }
-# Now that we have parsed the information for the link used audit records, let's track what happened to each link
+    Write-Host "Processing" $Records.Count "audit records..."
+    $Report = @() # Create output file for report
+    # Scan each audit record to extract information
+    ForEach ($Rec in $Records) {
+      $AuditData = ConvertFrom-Json $Rec.Auditdata
+      $ReportLine = [PSCustomObject][Ordered]@{
+      TimeStamp = Get-Date($AuditData.CreationTime) -format g
+      User      = $AuditData.UserId
+      Action    = $AuditData.Operation
+      Object    = $AuditData.ObjectId
+      IPAddress = $AuditData.ClientIP
+      Workload  = $AuditData.Workload
+      Site      = $AuditData.SiteUrl
+      FileName  = $AuditData.SourceFileName 
+      SortTime  = $AuditData.CreationTime }
+    $Report += $ReportLine }
+  # Now that we have parsed the information for the link used audit records, let's track what happened to each link
+  $RecNo = 0; CLS; $TotalRecs = $Report.Count
   ForEach ($R in $Report) {
-     Write-Host "Examining records for" $R.FileName
+     $RecNo++
+     $ProgressBar = "Processing audit records for " + $R.FileName + " (" + $RecNo + " of " + $TotalRecs + ")" 
+     Write-Progress -Activity "Checking Sharing Activity With Anonymous Links" -Status $ProgressBar -PercentComplete ($RecNo/$TotalRecs*100)
      $StartSearch = $R.TimeStamp; $EndSearch = (Get-Date $R.TimeStamp).AddDays(+7) # We'll search for any audit records 
      $AuditRecs = (Search-UnifiedAuditLog -StartDate $StartSearch -EndDate $EndSearch -IPAddresses $R.IPAddress -Operations FileAccessedExtended, FilePreviewed, FileModified, FileAccessed, FileDownloaded -ResultSize 100)
      Foreach ($AuditRec in $AuditRecs) {
@@ -40,5 +43,6 @@ ForEach ($Rec in $Records) {
             FileName  = $AuditData.SourceFileName 
             SortTime  = $AuditData.CreationTime }}
          $Report += $ReportLine }
-}
+}}
 $Report | Sort FileName, IPAddress, User, SortTime | Export-CSV -NoTypeInformation "c:\Temp\AnonymousLinksUsed.CSV"
+Write-Host "All done. Output file is available in c:\temp\AnonymousLinksUsed.Csv"
