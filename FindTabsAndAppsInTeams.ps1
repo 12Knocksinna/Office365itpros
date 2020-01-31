1920855b9445d2b79052b813467fa09772a184ad
Tony Redmond <45732130+12Knocksinna@users.noreply.github.com>
2020-01-31 17:50:29 +0000

    Update FindTabsAndAppsInTeams.ps1

--- FindTabsAndAppsInTeams.ps1
+++ FindTabsAndAppsInTeams.ps1
@@ -1,8 +1,10 @@
+# https://github.com/12Knocksinna/Office365itpros/blob/master/FindTabsAndAppsInTeams.ps1
+# The correct tenant values for the app id etc. are in this script.
 Cls
 # Define the values applicable for the application used to connect to the Graph
-$AppId = "d716b32c-0edb-48be-9385-30a9cfd96154" # Will be different for your tenant
-$TenantId = "b662313f-14fc-43a2-9a7a-d2e27f5f3478" # Will also be different for your tenant
-$AppSecret = 's_rkvIn1oZ1cNceUBvJ2or1lrrIsb*:=' #And you'll need to get this too
+$AppId = "d716b32c-0edb-48be-9385-30a9cfd96153"    # Change this
+$TenantId = "b662313f-14fc-43a2-9a7a-d2e27f4f3476" # And this
+$AppSecret = 's_rkvIn1oZ1cNceUBvJ2or1lrrIsb*:='    # and this
 
 # Construct URI and body needed for authentication
 $uri = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token"
@@ -26,37 +28,55 @@ $ctype = "application/json"
 
 # Create list of Teams in the tenant
 Write-Host "Fetching list of Teams in the tenant"
+# This call only returns 100 teams
+# $Teams = Invoke-WebRequest -Method GET -Uri "$($uri)groups?`$filter=resourceProvisioningOptions/Any(x:x eq 'Team')" -ContentType $ctype -Headers $headers | ConvertFrom-Json
+# This is how to paginate through and build a hashtable containing all the teams in a tenant using the NextLink
 $Teams = Invoke-WebRequest -Method GET -Uri "$($uri)groups?`$filter=resourceProvisioningOptions/Any(x:x eq 'Team')" -ContentType $ctype -Headers $headers | ConvertFrom-Json
+$TeamsHash = @{}
+$Teams.Value.ForEach( {
+   $TeamsHash.Add($_.Id, $_.DisplayName) } )
+$NextLink = $Teams.'@Odata.NextLink'
+While ($NextLink -ne $Null) {
+   $Teams = Invoke-WebRequest -Method GET -Uri $NextLink -ContentType $ctype -Headers $headers | ConvertFrom-Json
+   $Teams.Value.ForEach( {
+      $TeamsHash.Add($_.Id, $_.DisplayName) } )
+   $NextLink = $Teams.'@odata.NextLink' }
+
+# All teams found...
+Write-Host "Processing" $TeamsHash.Count "Teams..."
 
 $Report = [System.Collections.Generic.List[Object]]::new() # Create output file for report; $ReportLine = $Null
 $i = 0
 # Loop through each team to examine its channels and discover if any are email-enabled
-ForEach ($Team in $Teams.Value) {
+ForEach ($Team in $TeamsHash.Keys) {
       $i++
-      $ProgressBar = "Processing Team " + $Team.DisplayName + " (" + $i + " of " + $Teams.Value.Count + ")"
-      Write-Progress -Activity "Checking Teams Information" -Status $ProgressBar -PercentComplete ($i/$Teams.Value.Count*100)
+      $TeamId = $($Team); $TeamDisplayName = $TeamsHash[$Team]  #Populate variables to identify the team
+      $ProgressBar = "Processing Team " + $TeamDisplayName + " (" + $i + " of " + $TeamsHash.Count + ")"
+      Write-Progress -Activity "Checking Teams Information" -Status $ProgressBar -PercentComplete ($i/$TeamsHash.Count*100)
       # Get apps installed in the team
-      $teamApps = Invoke-WebRequest -Headers $Headers -Uri "https://graph.microsoft.com/v1.0/teams/$($Team.id)/installedApps?`$expand=teamsApp" -ErrorAction Stop
-     $teamApps = ($teamApps.content | ConvertFrom-Json).Value
-     $TeamAppNumber = 0
-     ForEach ($App in $TeamApps) {
+      $TeamApps = Invoke-WebRequest -Headers $Headers -Uri "https://graph.microsoft.com/v1.0/teams/$($TeamId)/installedApps?`$expand=teamsApp" -ErrorAction Stop
+      $TeamApps = ($TeamApps.content | ConvertFrom-Json).Value
+      $TeamAppNumber = 0
+      ForEach ($App in $TeamApps) {
          $TeamAppNumber++
          $ReportLine = [PSCustomObject][Ordered]@{
-            Record = "App"
-            Number = $TeamAppNumber
-            Team   = $Team.DisplayName
-            TeamId = $Team.Id
-            App    = $App.TeamsApp.DisplayName
-            AppId  = $App.TeamsApp.Id
-            Distribution = $App.TeamsApp.DistributionMethod }
+            Record  = "App"
+            Number  = $TeamAppNumber
+            Team    = $TeamDisplayName
+            TeamId  = $TeamId
+            Channel = "N/A"
+            App     = $App.TeamsApp.DisplayName
+            AppId   = $App.TeamsApp.Id
+            Distribution = $App.TeamsApp.DistributionMethod 
+            WebURL  = "N/A" }
         $Report.Add($ReportLine) }
             
 # Get the channels so we can report the tabs created in each channel
- $TeamChannels = Invoke-WebRequest -Headers $Headers -Uri "https://graph.microsoft.com/beta/Teams/$($Team.id)/channels" -ErrorAction Stop
-    $TeamChannels = ($TeamChannels.Content | ConvertFrom-Json).value
+ $TeamChannels = Invoke-WebRequest -Headers $Headers -Uri "https://graph.microsoft.com/beta/Teams/$($TeamId)/channels" -ErrorAction Stop
+ $TeamChannels = ($TeamChannels.Content | ConvertFrom-Json).value
 # Find the tabs created for each channel (standard tabs like Files don't show up here)    
-ForEach ($Channel in $TeamChannels) {
-        $Tabs = Invoke-WebRequest -Headers $Headers -Uri "https://graph.microsoft.com/beta/teams/$($Team.id)/channels/$($channel.id)/tabs?`$expand=teamsApp" 
+     ForEach ($Channel in $TeamChannels) {
+        $Tabs = Invoke-WebRequest -Headers $Headers -Uri "https://graph.microsoft.com/beta/teams/$($TeamId)/channels/$($channel.id)/tabs?`$expand=teamsApp" 
         $Tabs = ($Tabs.Content | ConvertFrom-Json).value
         $TabNumber = 0
         ForEach ($Tab in $Tabs) {
@@ -64,8 +84,8 @@ ForEach ($Channel in $TeamChannels) {
             $ReportLine = [PSCustomObject][Ordered]@{
               Record  = "Channel tab"
               Number  = $TabNumber
-              Team    = $Team.DisplayName
-              TeamId  = $Team.Id
+              Team    = $TeamDisplayName
+              TeamId  = $TeamId
               Channel = $Channel.DisplayName
               Tab     = $Tab.DisplayName
               AppId   = $Tab.TeamsApp.Id
@@ -74,5 +94,5 @@ ForEach ($Channel in $TeamChannels) {
         $Report.Add($ReportLine) }}
 }
 
-$Report | Sort Team | Export-CSV C:\Temp\TeamsChannelsWithEmailAddress.Csv -NoTypeInformation
-Write-Host $EmailAddresses "mail-enabled channels found. Details are in C:\Temp\TeamsChannelsWithEmailAddress.Csv"
+$Report | Sort Team | Export-CSV C:\Temp\TeamsChannelsAndAppsCsv -NoTypeInformation
+Write-Host $TeamsHash.Count "teams processed. Details are in C:\Temp\TeamsChannelsAndApps.Csv"
