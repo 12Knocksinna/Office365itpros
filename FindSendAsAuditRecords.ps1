95c613b94cffbcf1d8a4417c5e01b86e2e1e26d5
Tony Redmond <45732130+12Knocksinna@users.noreply.github.com>
2020-02-10 22:32:14 +0000

    Create FindSendAsAuditRecords.ps1
    First version...

---
+++ FindSendAsAuditRecords.ps1
@@ -0,0 +1,51 @@
+# A script to find SendAs records in the Office 365 audit log and identify events belonging to user/shared mailboxes
+# and those belonging to group mailboxes/Teams
+# V1.0 10 Feb 2020
+# First populate the Recipients Hash Table with user mailboxes, group mailboxes, and shared mailboxes
+CLS
+Write-Host "Populating Recipients Table..."
+$RecipientsTable = @{}
+Try {
+    $Recipients = Get-Mailbox -ResultSize Unlimited -RecipientTypeDetails UserMailbox, SharedMailbox}
+Catch {
+    Write-Host "Can't find recipients" ; break}
+# Now Populate hash table with label data  
+$Recipients.ForEach( {
+       $RecipientsTable.Add([String]$_.PrimarySmtpAddress, $_.RecipientTypeDetails) } )
+# And include group mailboxes
+$GroupMailboxes = Get-Mailbox -ResultSize Unlimited -GroupMailbox
+$GroupMailboxes.ForEach( {
+       $RecipientsTable.Add([String]$_.PrimarySmtpAddress, $_.RecipientTypeDetails) } )
+Write-Host "Finding audit records for Send As operations..."
+# You might need to increase the number of retrieved records if your tenant generates lots of SendAs events
+$Records = (Search-UnifiedAuditLog -StartDate (Get-Date).AddDays(-90) -EndDate (Get-Date).AddDays(+1) -Operations "SendAs" -ResultSize 2000)
+If ($Records.Count -eq 0) {
+    Write-Host "No audit records for Send As found." }
+Else {
+    Write-Host "Processing" $Records.Count "Send As audit records..."
+    $Report = [System.Collections.Generic.List[Object]]::new() # Create output file 
+    # Scan each audit record to extract information
+    ForEach ($Rec in $Records) {
+      $AuditData = ConvertFrom-Json $Rec.Auditdata
+      $MailboxType = $RecipientsTable.Item($AuditData.MailboxOwnerUPN) # Look up hash table
+      If ($MailboxType -eq "GroupMailbox") {$Reason = "Group Mailbox Send"} Else {$Reason = "Delegate Send As"}
+      If ($AuditData.UserId -eq "S-1-5-18") {$UserId = "Service Account"} Else {$UserId = $AuditData.UserId}
+      $ReportLine = [PSCustomObject] @{
+           TimeStamp   = Get-Date($AuditData.CreationTime) -format g
+           SentBy      = $AuditData.MailboxOwnerUPN
+           SentAs      = $AuditData.SendAsUserSmtp
+           Subject     = $AuditData.Item.Subject
+           User        = $AuditData.UserId
+           Action      = $AuditData.Operation
+           Reason      = $Reason
+           UserType    = $AuditData.UserType
+           LogonType   = $AuditData.LogonType
+           ClientIP    = $AuditData.ClientIP
+           MailboxType = $MailboxType
+           ClientInfo  = $AuditData.ClientInfoString
+           Status      = $AuditData.ResultStatus }        
+      $Report.Add($ReportLine) }
+}
+$Report | ? {$_.MailboxType -eq "UserMailbox"} | Out-GridView
+$Report |Export-Csv -NoTypeInformation -Path c:\temp\SendASAuditRecords.csv
+Write-Host "Report File saved in" c:\temp\SendASAuditRecords.csv
