faab4ca6426c89cfa762229d4db60200b9ce9329
Tony Redmond <45732130+12Knocksinna@users.noreply.github.com>
2021-01-18 15:55:30 +0000

    Update UpdateOWASignatures.ps1
    Add disclaimer

--- UpdateOWASignatures.ps1
+++ UpdateOWASignatures.ps1
@@ -44,3 +44,9 @@ ForEach ($M in $Mbx) {
    Set-MailboxMessageConfiguration -Identity $M.UserPrincipalName -SignatureHTML $SignatureHTML -AutoAddSignature $True -AutoAddSignatureOnReply $False 
 }
 Write-Host "All done. Signatures updated for" $Mbx.count "mailboxes"
+
+# An example script used to illustrate a concept. More information about the topic can be found in the Office 365 for IT Pros eBook https://gum.co/O365IT/
+# and/or a relevant article on https://office365itpros.com or https://www.petri.com. See our post about the Office 365 for IT Pros repository # https://office365itpros.com/office-365-github-repository/ for information about the scripts we write.
+
+# Do not use our scripts in production until you are satisfied that the code meets the need of your organization. Never run any code downloaded from the Internet without
+# first validating the code in a non-production environment.
