d5ef235e466fa9c3bd4c977d0ab8d53732df0fc1
Tony Redmond <45732130+12Knocksinna@users.noreply.github.com>
2022-01-24 10:10:22 +0000

    Update FindObsoleteGuestsByActivityV2.ps1
    Remove duplicate text

--- FindObsoleteGuestsByActivityV2.ps1
+++ FindObsoleteGuestsByActivityV2.ps1
@@ -3,12 +3,6 @@
 # https://github.com/12Knocksinna/Office365itpros/blob/master/FindObsoleteGuestsByActivityV2.ps1
 # 
 # Start by finding all Guest Accounts
-
-# An example script used to illustrate a concept. More information about the topic can be found in the Office 365 for IT Pros eBook https://gum.co/O365IT/
-# and/or a relevant article on https://office365itpros.com or https://www.petri.com. See our post about the Office 365 for IT Pros repository # https://office365itpros.com/office-365-github-repository/ for information about the scripts we write.
-
-# Do not use our scripts in production until you are satisfied that the code meets the need of your organization. Never run any code downloaded from the Internet without
-# first validating the code in a non-production environment.
 $Guests = (Get-AzureADUser -Filter "UserType eq 'Guest'" -All $True| Select Displayname, UserPrincipalName, Mail, ObjectId)
 Write-Host $Guests.Count "guest accounts found. Checking their activity..."
 $StartDate = Get-Date(Get-Date).AddDays(-90) #For audit log
