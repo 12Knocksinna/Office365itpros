6c14f0dc7d65cdc06e1f6a4989b123fe1d18955d
Tony Redmond <45732130+12Knocksinna@users.noreply.github.com>
2023-01-13 18:41:13 +0000

    Update FindOldGuestUsers.ps1

--- FindOldGuestUsers.ps1
+++ FindOldGuestUsers.ps1
@@ -66,7 +66,7 @@ $PercentStale = ($StaleGuests/$GuestUsers.Count).toString("P")
 Write-Host ("Script complete. {0} guest accounts found aged over {1} days ({2} of total). Output CSV file is in {3}" -f $StaleGuests, $AgeThreshold, $PercentStale, $OutputReport)
 
 # An example script used to illustrate a concept. More information about the topic can be found in the Office 365 for IT Pros eBook https://gum.co/O365IT/
-# and/or a relevant article on https://office365itpros.com or https://www.petri.com. See our post about the Office 365 for IT Pros repository # https://office365itpros.com/office-365-github-repository/ for information about the scripts we write.
+# and/or a relevant article on https://office365itpros.com or https://www.practical365.com. See our post about the Office 365 for IT Pros repository # https://office365itpros.com/office-365-github-repository/ for information about the scripts we write.
 
 # Do not use our scripts in production until you are satisfied that the code meets the need of your organization. Never run any code downloaded from the Internet without
 # first validating the code in a non-production environment.
