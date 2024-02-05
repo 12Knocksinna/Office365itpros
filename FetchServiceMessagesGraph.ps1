99aa86558e3da8d5960332bcbb7a3a737c003983
Tony Redmond <45732130+12Knocksinna@users.noreply.github.com>
2024-02-05 17:33:05 +0000

    Updated for Graph SDK

--- FetchServiceMessagesGraph.ps1
+++ FetchServiceMessagesGraph.ps1
@@ -5,8 +5,10 @@
 
 Connect-MgGraph -NoWelcome -Scopes ServiceMessage.Read.All
 
+$CSVOutputFile = "c:\temp\MessageCenterPosts.csv"
+
 # Fetch information from Graph
-Write-Host "Fetching Office 365 Message Center Notifications..."
+Write-Host "Fetching Microsoft 365 Message Center Notifications..."
 [array]$MCPosts = Get-MgServiceAnnouncementMessage -Sort 'LastmodifiedDateTime desc' -All 
 
 # And Report what we find
@@ -49,6 +51,81 @@ ForEach ($M in $MCPosts) {
 
 $Report | Sort-Object {$_.'Last Update' -as [DateTime]} -Descending | `
    Select-Object MessageId, Title, Category, 'Last Update', 'Action Required By', Age | Out-GridView
+$Report | Export-CSV -NoTypeInformation $CSVOutputFile
+
+# Figure out delays
+$DelayedPosts = $Report | Where-Object {$_.Title -like "*(Updated)*"}
+[int]$TeamsDelays = 0; [int]$ExchangeDelays = 0; [int]$SharePointDelays = 0; [int]$OtherDelays = 0
+[int]$StreamDelays = 0; [int]$PlannerDelays = 0; [int]$IntuneDelays = 0; [int]$OneDriveDelays = 0; [int]$WebAppsDelays = 0
+[int]$VivaDelays = 0; [int]$M365AppsDelays = 0; [int]$M365SuiteDelays = 0; [int]$DefenderDelays = 0
+ForEach ($Delay in $DelayedPosts) {
+   Switch -wildcard ($Delay.Workloads) {
+      "*Teams*" {
+         $TeamsDelays++
+      }
+      "Exchange*" {
+         $ExchangeDelays++
+      }
+      "SharePoint*" {
+         $SharePointDelays++
+      }
+      "*Viva*" {
+         $VivaDelays++
+      }
+      "Microsoft 365 apps" {
+         $M365AppsDelays++
+      }
+      "*Stream*" {
+         $StreamDelays++
+      }
+      "*Defender*" {
+         $DefenderDelays++
+      }
+      "*Planner*" {
+         $PlannerDelays++
+      }
+      "*OneDrive*" {
+         $OneDriveDelays++
+      }
+      "Microsoft 365 suite" {
+         $M365SuiteDelays++
+      }
+      "*Intune*" {
+         $IntuneDelays++
+      }
+      "*web*" {
+         $WebAppsDelays++
+      }
+
+      Default {
+         $OtherDelays++
+      }
+   }
+}
+
+$PercentDelayed = ($DelayedPosts.count/$Report.count).toString('P')
+Write-Host ("{0} message center posts analyzed - output is in {1}." -f $MCPosts.count, $CSVOutputFile)
+Write-Host ""
+Write-Host ("Number of delayed posts: {0} ({1})" -f $DelayedPosts.count, $PercentDelayed)
+Write-Host ""
+Write-Host "Delayed Posts by Workload"
+Write-Host "-------------------------"
+Write-Host ""
+
+Write-Host ("Exchange Online           {0}" -f $ExchangeDelays)
+Write-Host ("Intune                    {0}" -f $IntuneDelays)
+Write-Host ("Microsoft 365 apps        {0}" -f $M365AppsDelays)
+Write-Host ("Microsoft 365 suite       {0}" -f $M365SuiteDelays)
+Write-Host ("Microsoft 365 for the web {0}" -f $WebAppsDelays)
+Write-Host ("Microsoft Defender        {0}" -f $DefenderDelays)
+Write-Host ("OneDrive for Business     {0}" -f $OneDriveDelays)
+Write-Host ("Planner                   {0}" -f $PlannerDelays)
+Write-Host ("SharePoint Online         {0}" -f $SharePointDelays)
+Write-Host ("Stream                    {0}" -f $StreamDelays)
+Write-Host ("Teams                     {0}" -f $TeamsDelays)
+Write-Host ("Viva                      {0}" -f $VivaDelays)
+Write-Host ("Other workloads           {0}" -f $OtherDelays)
+
 
 # An example script used to illustrate a concept. More information about the topic can be found in the Office 365 for IT Pros eBook https://gum.co/O365IT/
 # and/or a relevant article on https://office365itpros.com or https://www.practical365.com. See our post about the Office 365 for IT Pros repository # https://office365itpros.com/office-365-github-repository/ for information about the scripts we write.
